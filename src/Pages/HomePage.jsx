d9308d85c1403be403ed865851cda8019d9a75dc
Ilyas Isse <ilyasisse574@gmail.com>
2025-07-01 01:28:20 -0400

    Fix incorrect import path for Nav component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Nav from '../Components/nav';
+import Nav from '../Components/Nav';
 import Movies from '../Components/Movies';
-import MoviesNew from '../Components/UI/MoviesNew';
+
 
 
 const Home = () => {
